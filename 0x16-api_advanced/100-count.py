d25389eecec0f5c4566ea2cc4e2544ab7e66a2bb
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-11-29 23:35:40 +0300

    Commits

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,48 +1,97 @@
 #!/usr/bin/python3
-"""queries the Reddit API, parses the title of all hot articles, and prints a
-sorted count of given keywords(case-insensitive, delimited by spaces.
-Javascript should count as javascript, but java should not).
-"""
+''' Query reddit API recursively to obtain hot post titles'''
 import requests
-import operator
+import sys
 
 
-def count_words(subreddit, word_list=[], word_dict={}, after=None):
+def count_words(subreddit, word_list, word_dict={}, after="", first_call=True):
+    ''' sdf '''
+    url = 'https://api.reddit.com/r/{}/hot/?after={}'\
+          .format((subreddit), after)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
-        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-    }
-    r = requests.get('https://www.reddit.com/r/{:}/hot.json?after={:}'.format(
-        subreddit, after), headers=headers, allow_redirects=False)
-    if r.status_code == 200:
-        json = r.json()
-        data_dict = json.get('data')
-        post_list = data_dict.get('children')
-        for post in post_list:
-            post_data_dict = post.get('data')
-            for word in word_list:
-                title = post_data_dict.get('title').split()
-                title_copy = []
-                for j in title:
-                    title_copy.append(j.upper())
-                count = title_copy.count(word.upper())
-                if word_dict.get(word):
-                    word_dict[word] += count
-                else:
-                    word_dict[word] = count
-        after = data_dict.get('after')
-        if data_dict.get('after') is None:
-            sorted_list = sorted(word_dict.items(), key=operator.itemgetter(1),
-                                 reverse=True)
-            for i in range(len(sorted_list) - 1):
-                if sorted_list[i][1] == sorted_list[i+1][1]:
-                    if sorted_list[i][0] > sorted_list[i+1][0]:
-                        sorted_list[i], sorted_list[i+1] = sorted_list[
-                            i+1], sorted_list[i]
-            for item in sorted_list:
-                if item[1] > 0:
-                    print("{:}: {:}".format(item[0], item[1]))
-            return
-        return count_words(subreddit, word_list, word_dict, after)
-    else:
+                'User-Agent': 'My User Agent 1.0'
+              }
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    # check if invalid subreddit name
+    if r.status_code != 200:
+        print("\n")
         return
+    # convert and parse json data for the list of posts
+    content = r.json()
+    data = content['data']
+    dist = data['dist']
+    children = data['children']
+    after = data['after']
+    # call get_children that will find the title
+    word_dict = get_children(children, word_list,
+                             word_dict, 0, dist, first_call)
+    first_call = False
+    # if the current page it last page
+    if after is None:
+        print_final(word_dict, 0, word_list, len(word_list), 0)
+        return
+
+    # construct new url with the after value to get next page
+    url = 'https://api.reddit.com/r/{}/hot/?after={}'\
+          .format((subreddit), after)
+    word_dict = count_words(subreddit, word_list,
+                            word_dict, after, first_call)
+
+    print_final(word_dict, 0, word_list, len(word_list), 0)
+    return
+
+
+def get_children(children, word_list, word_dict,
+                 count, dist, first_call):
+    ''' get the title of each hot post'''
+
+    # retrieve title if there is content
+    if count < dist:
+        title = children[count]['data']['title']
+        title = title.lower()
+        title_list = title.split()
+        '''
+        if first_call is True:
+            for word in word_list:
+                word_dict[word] = 0
+        '''
+        word_dict = check_word(title_list, word_list, word_dict, 0, first_call)
+        count += 1
+        first_call = False
+        word_dict = get_children(children, word_list,
+                                 word_dict, count, dist, first_call)
+        return (word_dict)
+    else:
+        return word_dict
+
+
+def check_word(title_list, word_list,
+               word_dict, count, first_call):
+    ''' Check if one of the keyword is on a post title'''
+    if first_call is True:
+        for word in word_list:
+            word_dict[word] = 0
+    if count < len(word_list):
+        key = word_list[count]
+        if key.lower() in title_list:
+            word_dict[key] += 1
+        count += 1
+        first_call = False
+        word_dict = check_word(title_list,
+                               word_list, word_dict, count, first_call)
+    return (word_dict)
+
+
+def print_final(word_dict, count, word_list, length, x):
+    if count < length:
+        key = word_list[0]
+        if key in word_dict.keys():
+            value = word_dict[key]
+            if value != 0:
+                x = 1
+                print("{}: {}".format(key, value))
+        word_list.remove(key)
+        count += 1
+        print_final(word_dict, count, word_list, length, x)
+        if x == 0:
+            print('\n')
